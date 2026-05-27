001e024026df011aca724a226cd46a0877d20054
MahinourAbdelgawad <mahinourabdelgawad@aucegypt.edu>
2026-05-27 14:40:10 +0300

    changed draw landmark function to the one in docs

--- src/modules/arm_tracker.py
+++ src/modules/arm_tracker.py
@@ -57,33 +57,49 @@ class ArmTracker:
         return np.array([landmark.x, landmark.y, landmark.z])
     
 
-    def _draw_landmarks(self, frame, result):
-        RIGHT_ARM_CONNECTIONS = frozenset([
-                (12, 14),  
-                (14, 16),
-            ])
+    # def _draw_landmarks(self, frame, result):
+    #     RIGHT_ARM_CONNECTIONS = frozenset([
+    #             (12, 14),  
+    #             (14, 16),
+    #         ])
         
-        annotated_image = np.copy(frame)
-        h, w = frame.shape[:2]
+    #     annotated_image = np.copy(frame)
+    #     h, w = frame.shape[:2]
 
-        for pose_landmarks in result.pose_landmarks:
-            for start_idx, end_idx in RIGHT_ARM_CONNECTIONS:
-                p1 = pose_landmarks[start_idx]
-                p2 = pose_landmarks[end_idx]
-                if p1.visibility > 0.5 and p2.visibility > 0.5:
-                    x1, y1 = int(p1.x * w), int(p1.y * h)
-                    x2, y2 = int(p2.x * w), int(p2.y * h)
-                    cv.line(annotated_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    #     for pose_landmarks in result.pose_landmarks:
+    #         for start_idx, end_idx in RIGHT_ARM_CONNECTIONS:
+    #             p1 = pose_landmarks[start_idx]
+    #             p2 = pose_landmarks[end_idx]
+    #             if p1.visibility > 0.5 and p2.visibility > 0.5:
+    #                 x1, y1 = int(p1.x * w), int(p1.y * h)
+    #                 x2, y2 = int(p2.x * w), int(p2.y * h)
+    #                 cv.line(annotated_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-            for idx in [12, 14, 16]:
-                lm = pose_landmarks[idx]
-                if lm.visibility > 0.5:
-                    cx, cy = int(lm.x * w), int(lm.y * h)
-                    cv.circle(annotated_image, (cx, cy), 6, (0, 0, 255), -1)
-                    cv.circle(annotated_image, (cx, cy), 6, (255, 255, 255), 1)
+    #         for idx in [12, 14, 16]:
+    #             lm = pose_landmarks[idx]
+    #             if lm.visibility > 0.5:
+    #                 cx, cy = int(lm.x * w), int(lm.y * h)
+    #                 cv.circle(annotated_image, (cx, cy), 6, (0, 0, 255), -1)
+    #                 cv.circle(annotated_image, (cx, cy), 6, (255, 255, 255), 1)
+
+    #     return annotated_image
+    
+    def _draw_landmarks(self, rgb_image, detection_result):
+        pose_landmarks_list = detection_result.pose_landmarks
+        annotated_image = np.copy(rgb_image)
+
+        pose_landmark_style = drawing_styles.get_default_pose_landmarks_style()
+        pose_connection_style = drawing_utils.DrawingSpec(color=(0, 255, 0), thickness=2)
+
+        for pose_landmarks in pose_landmarks_list:
+            drawing_utils.draw_landmarks(
+                image=annotated_image,
+                landmark_list=pose_landmarks,
+                connections=vision.PoseLandmarksConnections.POSE_LANDMARKS,
+                landmark_drawing_spec=pose_landmark_style,
+                connection_drawing_spec=pose_connection_style)
 
         return annotated_image
-    
 
     def track(self):
         try:
