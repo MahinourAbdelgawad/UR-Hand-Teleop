1519d4bc92a8dc7350a68085df2b3ae9de7edba6
MahinourAbdelgawad <mahinourabdelgawad@aucegypt.edu>
2026-06-08 01:15:49 +0300

    added robotiq gripper

--- src/modules/mujoco_wrapper.py
+++ src/modules/mujoco_wrapper.py
@@ -3,15 +3,22 @@ import mujoco.viewer
 import numpy as np
 
 class MujocoWrapper:
-    def __init__(self, model = "arm_models/ur5e_model/ur5e.xml"):
+    def __init__(self, model = "arm_models/ur5e_model/ur5e.xml", gripper = "arm_models/robotiq_2f85/2f85.xml"):
         try:
-            self.model = mujoco.MjModel.from_xml_path(model)
+            # self.model = mujoco.MjModel.from_xml_path(model)
+            self.model = self._build_combined_model(
+                model,
+                gripper
+            )
             self.data = mujoco.MjData(self.model)
 
             # set default pose
-            # TODO: tweak this default pose
-            self.data.qpos[:6] = [0, -np.pi/4, -np.pi/2, -np.pi/4, np.pi/2, 0]
-            self.data.ctrl[:6] = [0, -np.pi/4, -np.pi/2, -np.pi/4, np.pi/2, 0]
+            # # TODO: tweak this default pose
+            # self.data.qpos[:6] = [0, -np.pi/4, -np.pi/2, -np.pi/4, np.pi/2, 0]
+            # self.data.ctrl[:6] = [0, -np.pi/4, -np.pi/2, -np.pi/4, np.pi/2, 0]
+
+            # self.data.ctrl[6] = 0 # gripper actuator open
+
             mujoco.mj_forward(self.model, self.data)
 
             self.viewer = None
@@ -65,7 +72,40 @@ class MujocoWrapper:
                 self.viewer.close()
 
         except Exception as e:
-            print(f"Error closing Mujoco Viewer: {e}")      
+            print(f"Error closing Mujoco Viewer: {e}")
+            
+
+    def _get_body(self, parent_body, target_name):
+        if parent_body.name == target_name:
+            return parent_body
+        
+        child = parent_body.first_body()
+        while child is not None:
+            found = self._get_body(child, target_name)
+            if found:
+                return found
+            
+            # MjSpec uses this pattern to iterate through siblings
+            child = parent_body.next_body(child)
+            
+        return None
+
+    def _build_combined_model(self, ur5e_path, gripper_path):
+        arm = mujoco.MjSpec.from_file(ur5e_path)
+        gripper = mujoco.MjSpec.from_file(gripper_path)
+
+        wrist = self._get_body(arm.worldbody, 'wrist_3_link')
+        if wrist is None:
+            raise ValueError("Could not find 'wrist_3_link' in the arm model.")
+
+        frame = wrist.add_frame()
+        frame.name = 'attachment_frame'
+        
+        arm.attach(gripper, frame=frame, prefix='gripper_')
+
+        return arm.compile()
+
+
      
 
 
--- src/tests/mujoco_test.py
+++ src/tests/mujoco_test.py
@@ -1,7 +1,43 @@
 import mujoco
 import mujoco.viewer
 
-model = mujoco.MjModel.from_xml_path('arm_models/ur5e_model/ur5e.xml')
+def get_body(parent_body, target_name):
+    if parent_body.name == target_name:
+        return parent_body
+    
+    child = parent_body.first_body()
+    while child is not None:
+        found = get_body(child, target_name)
+        if found:
+            return found
+        
+        # MjSpec uses this pattern to iterate through siblings
+        child = parent_body.next_body(child)
+        
+    return None
+
+def build_combined_model(ur5e_path, gripper_path):
+    arm = mujoco.MjSpec.from_file(ur5e_path)
+    gripper = mujoco.MjSpec.from_file(gripper_path)
+
+    wrist = get_body(arm.worldbody, 'wrist_3_link')
+    if wrist is None:
+        raise ValueError("Could not find 'wrist_3_link' in the arm model.")
+
+    frame = wrist.add_frame()
+    frame.name = 'attachment_frame'
+    
+    arm.attach(gripper, frame=frame, prefix='gripper_')
+
+    return arm.compile()
+
+
+# model = mujoco.MjModel.from_xml_path('arm_models/ur5e_model/ur5e.xml')
+model = build_combined_model(
+    'arm_models/ur5e_model/ur5e.xml',
+    'arm_models/robotiq_2f85/2f85.xml'
+)
+
 data = mujoco.MjData(model)
 
 with mujoco.viewer.launch_passive(model, data) as v:
@@ -11,3 +47,5 @@ with mujoco.viewer.launch_passive(model, data) as v:
         mujoco.mj_step(model, data)
         v.sync()
         time.sleep(0.01)
+
+
